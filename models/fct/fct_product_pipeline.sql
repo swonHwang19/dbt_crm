b9492311d0510fd5551e77258a488ec036620bea
Sangwon Hwang <sangwon@mz.co.kr>
2024-11-13 05:11:38 +0000

    fix9

--- models/fct/fct_product_pipeline.sql
+++ models/fct/fct_product_pipeline.sql
@@ -8,9 +8,8 @@ WITH fct_product_sales AS (
 SELECT
     product_name,
     SUM(quantity) AS total_quantity_sold,
-    price,
     SUM(quantity * price) AS total_revenue
 
 FROM fct_product_sales
-GROUP BY product_name, price
+GROUP BY product_name
 ORDER BY total_revenue DESC
